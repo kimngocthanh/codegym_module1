bb6fb53e9723988de9699e682030a731c10f2678
kimngocthanh <63952549+kimngocthanh@users.noreply.github.com>
2023-03-08 15:26:26 +0700

    e nop bai tap

---
+++ codegym_week_2/thuc_hanh_tuan_2/TH_bien/bien.js
@@ -0,0 +1,24 @@
+/*
+let i =10
+let f =20.5
+let b = true
+let s ='ha noi'
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f= ' + f)
+document.write('<br>')
+document.write('b='+b)
+document.write('<br>')
+document.write('s='+s)
+*/
+/*
+let a =20
+let b=10
+let S= a * b
+document.write('S=' + S )
+*/
+let a = prompt()
+let b = prompt()
+let S = a * b
+alert(S)
